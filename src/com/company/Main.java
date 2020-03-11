db84487200fea44df1ff6d06eba980a3b85027af
JulianCamacho <jjulian.341@gmail.com>
2020-03-11 17:48:15 -0600

    Generar el .jar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,6 @@ package src.com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
-    }
-
     public int suma(int a, int b){
         return a+b;
     }
